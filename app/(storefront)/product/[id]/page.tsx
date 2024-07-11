7263c46576f6f9b5b319c98c2ef94dbb627044ee
Mohamad44p <mohamad44p@yahoo.com>
2024-07-11 16:35:58 +0300

    Edit product page text color and postions

--- app/(storefront)/product/[id]/page.tsx
+++ app/(storefront)/product/[id]/page.tsx
@@ -42,10 +42,10 @@ export default async function ProductIdRoute({
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start lg:gap-x-24 py-6">
         <ImageSlider images={data.images} />
         <div>
-          <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
+          <h1 className="text-3xl font-extrabold tracking-tight">
             {data.name}
           </h1>
-          <p className="text-3xl mt-2 text-gray-900">${data.price}</p>
+          <p className="text-3xl mt-2">${data.price}</p>
           <div className="mt-3 flex items-center gap-1">
             <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
             <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
@@ -53,7 +53,9 @@ export default async function ProductIdRoute({
             <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
             <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
           </div>
-          <p className="text-base text-gray-700 mt-6">{data.description}</p>
+          <p className="text-base text-muted-foreground mt-6">
+            {data.description}
+          </p>
 
           <form action={addProducttoShoppingCart}>
             <ShoppingBagButton />
